7855962a22f75f9b89d69a8c7674877a6c64a287
Roman <romashka.ross@gmail.com>
2019-10-20 14:49:51 +0700

    v1.1.2

--- main.cpp
+++ main.cpp
@@ -1,10 +1,63 @@
 #include <iostream>
 #include <fstream>
 #include <vector>
+#include <algorithm>
+
+
+std::vector<std::vector<double>> formxyz(const std::vector<double >& x, const std::vector<double >& y, const std::vector<double >& z){
+    std::ofstream fout("xyz.txt");
+    std::vector<std::vector<double>> tmp;
+    int count = 0;
+    for(double iz : z){
+        for(double iy : y){
+            for (double ix : x) {
+                std::vector<double> tmpxyz;
+                tmpxyz.emplace_back(ix);
+                tmpxyz.emplace_back(iy);
+                tmpxyz.emplace_back(iz);
+                tmp.emplace_back(tmpxyz);
+                fout << count++ << ' ' << ix << ' ' << iy << ' ' << iz << std::endl;
+            }
+        }
+    }
+    fout.close();
+    return tmp;
+}
+
+std::vector<std::vector<double>> formnvtr(const std::vector<double >& x, const std::vector<double >& y, const std::vector<double >& z){
+    std::vector<std::vector<double>> tmp;
+    std::vector<double> a;
+    std::ofstream fout("nvtr.txt");
+    int xsize = x.size();
+    int ysize = y.size();
+    int zsize = z.size();
+    int count = 0;
+    for(size_t iz = 0; iz < zsize - 1; ++iz) {
+        count = (int)iz * xsize * ysize;
+        for (size_t iy = 0; iy < ysize - 1; ++iy) {
+            count += (int)iy * xsize;
+            for (size_t ix = 0; ix < xsize - 1; ++ix) {
+                fout << a.emplace_back(ix + count) << ' '
+                     << a.emplace_back(ix + count + 1) << ' '
+                     << a.emplace_back(ix + xsize + count) << ' '
+                     << a.emplace_back(ix + xsize + count + 1) << ' '
+                     << a.emplace_back(ix + count + xsize * ysize) << ' '
+                     << a.emplace_back(ix + count + xsize * ysize + 1) << ' '
+                     << a.emplace_back(ix + xsize + xsize * ysize + count) << ' '
+                     << a.emplace_back(ix + xsize + xsize * ysize + count + 1) << std::endl;
+                tmp.push_back(a);
+                a.clear();
+            }
+        }
+    }
+    fout.close();
+    return tmp;
+}
 
 
 int main() {
     std::ifstream fin("x.txt");
+
     std::vector<double> x, y, z;
     while(!fin.eof()){
         double tmp;
@@ -25,41 +78,38 @@ int main() {
         fin >> tmp;
         z.emplace_back(tmp);
     }
+    fin.close();
 
-    std::ofstream fout("xyz.txt");
+    std::vector<std::vector<double>> xyz = formxyz(x, y, z);
 
-    int count = 0;
-    for(double iz : z){
-        for(double iy : y){
-            for (double ix : x) {
-                fout << count++ << ' ' << ix << ' ' << iy << ' ' << iz << std::endl;
+    std::vector<std::vector<double>> nvtr = formnvtr(x, y, z);
+
+    std::vector<std::vector<double>> listOfConnections;
+
+    int count = x.size() * y.size() * z.size();
+    for(size_t i = 0; i < count; ++i){
+        std::vector<double> a;
+        listOfConnections.push_back(a);
+    }
+
+    for(const auto& element : nvtr){
+        for(size_t i = 0; i < element.size(); ++i){
+            for(size_t j = 0; j < i; ++j){
+                listOfConnections[element[i]].push_back(element[j]);
             }
         }
     }
-    fout.close();
-
-    fout.open("nvtr.txt");
-
-    int xsize = x.size();
-    int ysize = y.size();
-    int zsize = z.size();
-    int tmp = 0;
-    for(size_t iz = 0; iz < zsize - 1; ++iz) {
-        tmp = iz * xsize * ysize;
-        for (size_t iy = 0; iy < ysize - 1; ++iy) {
-            tmp += iy * xsize;
-            for (size_t ix = 0; ix < xsize - 1; ++ix) {
-                fout << ix + tmp << ' '
-                << ix + tmp + 1 << ' '
-                << ix + xsize + tmp << ' '
-                << ix + xsize + tmp + 1 << ' '
-                << ix + tmp + xsize * ysize << ' '
-                << ix + tmp + xsize * ysize + 1 << ' '
-                << ix + xsize + xsize * ysize + tmp << ' '
-                << ix + xsize + xsize * ysize + tmp + 1 << std::endl;
+    for (auto & item : listOfConnections) {
+        std::sort(item.begin(), item.end());
+    }
+    for(auto & item : listOfConnections){
+        for(auto i = ++item.begin(); i < item.end(); ++i){
+            if(*i == *(i - 1)){
+                item.erase(i);
             }
         }
     }
+
 
     return 0;
 }
